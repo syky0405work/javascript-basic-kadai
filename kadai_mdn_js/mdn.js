0a4c72f669c416eb4578b255f00fa1d33e5df3df
syky0405work <154429021+syky0405work@users.noreply.github.com>
2024-01-06 12:48:46 +0900

    Update mdn.js

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -5,7 +5,7 @@ const year = today.getFullYear();
 
 const day = today.getDate();
 
-const month = today.getMonth();
+const month = today.getMonth() + 1;
 
 console.log(year + "年" + month + "月" + day + "日");
 
